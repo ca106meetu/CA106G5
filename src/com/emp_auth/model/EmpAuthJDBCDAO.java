2c17a8477ff414ca4d5e32413a0187b03d9d76ce
JAVA <JAVA@DESKTOP-NTQ423V>
2019-02-12 23:15:41 +0800

    jackhsu190212V2315

--- src/com/emp_auth/model/EmpAuthJDBCDAO.java
+++ src/com/emp_auth/model/EmpAuthJDBCDAO.java
@@ -3,6 +3,8 @@ package com.emp_auth.model;
 import java.sql.*;
 import java.util.*;
 
+import com.auth.model.AuthVO;
+
 public class EmpAuthJDBCDAO implements EmpAuthDAO_interface{
 	
 	String driver = "oracle.jdbc.driver.OracleDriver";
@@ -22,28 +24,232 @@ public class EmpAuthJDBCDAO implements EmpAuthDAO_interface{
 		"UPDATE EMPAUTH set AUTH_ID=? where EMP_ID=?";
 	@Override
 	public void insert(EmpAuthVO empAuthVO) {
-		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(INSERT_STMT);
+			
+			pstmt.setString(1, empAuthVO.getEmp_id());
+			pstmt.setString(2, empAuthVO.getAuth_id());
+	
+			pstmt.executeUpdate();
+			
+			
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
 		
 	}
 	@Override
 	public void update(EmpAuthVO empAuthVO) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
 		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(UPDATE);
+			
+			pstmt.setString(1, empAuthVO.getAuth_id());
+			pstmt.setString(2, empAuthVO.getEmp_id());
+						
+			pstmt.executeUpdate();
+			
+			
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
 		
 	}
 	@Override
 	public void delete(String emp_id, String auth_id) {
-		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(DELETE);
+			
+			pstmt.setString(1, emp_id);
+			pstmt.setString(2, auth_id);
+			pstmt.executeUpdate();
+			
+			
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
 		
 	}
 	@Override
 	public EmpAuthVO findByPrimaryKey(String emp_id, String auth_id) {
-		// TODO Auto-generated method stub
-		return null;
+		EmpAuthVO empAuthVO = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(GET_ONE_STMT);
+			pstmt.setString(1, emp_id);
+			pstmt.setString(2, auth_id);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				empAuthVO = new EmpAuthVO();
+				empAuthVO.setEmp_id(rs.getString("emp_id"));
+				empAuthVO.setAuth_id(rs.getString("auth_id"));			
+			}
+						
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return empAuthVO;
 	}
 	@Override
 	public List<EmpAuthVO> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<EmpAuthVO> list = new ArrayList<>();
+		EmpAuthVO empAuthVO = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(GET_ALL_STMT);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				empAuthVO = new EmpAuthVO();
+				empAuthVO.setEmp_id(rs.getString("emp_id"));
+				empAuthVO.setAuth_id(rs.getString("auth_id"));
+				
+				list.add(empAuthVO);
+			}
+			
+			
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return list;
 	}
 	
 }
