ac1d55e7cfc18b8f991d4014eb4fbf2d0fca1301
JAVA <JAVA@DESKTOP-NTQ423V>
2019-02-13 00:56:23 +0800

    jackhsu190213V0100

--- src/com/emp/model/EmpJDBCDAO.java
+++ src/com/emp/model/EmpJDBCDAO.java
@@ -1,27 +1,31 @@
-package com.emp_auth.model;
+package com.emp.model;
 
 import java.sql.*;
 import java.util.*;
 
-public class EmpAuthJDBCDAO implements EmpAuthDAO_interface{
-	
+import com.emp_auth.model.EmpAuthVO;
+
+public class EmpJDBCDAO implements EmpDAO_interface {
+
+
 	String driver = "oracle.jdbc.driver.OracleDriver";
 	String url = "jdbc:oracle:thin:@localhost:1521:XE";
 	String userid = "CA106G5";
 	String passwd = "123456";
 
 	private static final String INSERT_STMT = 
-		"INSERT INTO EMPAUTH (EMP_ID, AUTH_ID) VALUES (?, ?)";
+		"INSERT INTO EMP (EMP_ID, EMP_PW, EMP_NAME, EMP_BDAY, EMP_EMAIL, EMP_PHO, EMP_GEND, EMP_PIC, EMP_STATE, EMP_HDAY, EMP_ADDRESS) VALUES "
+		+ " ('E'||LPAD(to_char(emp_seq.NEXTVAL), 6, '0'), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 	private static final String GET_ALL_STMT = 
-		"SELECT * FROM EMPAUTH";
+		"SELECT * FROM EMP";
 	private static final String GET_ONE_STMT = 
-		"SELECT * FROM EMPAUTH where EMP_ID = ? AND AUTH_ID = ?";
+		"SELECT * FROM EMP where EMP_ID = ?";
 	private static final String DELETE = 
-		"DELETE FROM EMPAUTH where EMP_ID = ? AND AUTH_ID = ?";
+		"DELETE FROM EMP where EMP_ID=? ";
 	private static final String UPDATE = 
-		"UPDATE EMPAUTH set AUTH_ID=? where EMP_ID=?";
+		"UPDATE EMP set EMP_PW=?, EMP_NAME=?, EMP_BDAY=?, EMP_EMAIL=?, EMP_PHO=?, EMP_GEND=?, EMP_PIC=?, EMP_STATE=?, EMP_HDAY=?, EMP_ADDRESS=? where EMP_ID=?";
 	@Override
-	public void insert(EmpAuthVO empAuthVO) {
+	public void insert(EmpVO empVO) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		
@@ -30,8 +34,16 @@ public class EmpAuthJDBCDAO implements EmpAuthDAO_interface{
 			con = DriverManager.getConnection(url, userid, passwd);
 			pstmt = con.prepareStatement(INSERT_STMT);
 			
-			pstmt.setString(1, empAuthVO.getEmp_id());
-			pstmt.setString(2, empAuthVO.getAuth_id());
+			pstmt.setString(1, empVO.getEmp_pw());
+			pstmt.setString(2, empVO.getEmp_name());
+			pstmt.setDate(3, empVO.getEmp_bday());
+			pstmt.setString(4, empVO.getEmp_email());
+			pstmt.setString(5, empVO.getEmp_pho());
+			pstmt.setString(6, empVO.getEmp_gend());
+			pstmt.setBytes(7, empVO.getEmp_pic());
+			pstmt.setInt(8, empVO.getEmp_state());
+			pstmt.setDate(9, empVO.getEmp_hday());
+			pstmt.setString(10, empVO.getEmp_address());
 	
 			pstmt.executeUpdate();
 			
@@ -61,8 +73,9 @@ public class EmpAuthJDBCDAO implements EmpAuthDAO_interface{
 		}
 		
 	}
+	
 	@Override
-	public void update(EmpAuthVO empAuthVO) {
+	public void update(EmpVO empVO) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		
@@ -71,8 +84,17 @@ public class EmpAuthJDBCDAO implements EmpAuthDAO_interface{
 			con = DriverManager.getConnection(url, userid, passwd);
 			pstmt = con.prepareStatement(UPDATE);
 			
-			pstmt.setString(1, empAuthVO.getAuth_id());
-			pstmt.setString(2, empAuthVO.getEmp_id());
+			pstmt.setString(1, empVO.getEmp_pw());
+			pstmt.setString(2, empVO.getEmp_name());
+			pstmt.setDate(3, empVO.getEmp_bday());
+			pstmt.setString(4, empVO.getEmp_email());
+			pstmt.setString(5, empVO.getEmp_pho());
+			pstmt.setString(6, empVO.getEmp_gend());
+			pstmt.setBytes(7, empVO.getEmp_pic());
+			pstmt.setInt(8, empVO.getEmp_state());
+			pstmt.setDate(9, empVO.getEmp_hday());
+			pstmt.setString(10, empVO.getEmp_address());
+			pstmt.setString(11, empVO.getEmp_id());
 						
 			pstmt.executeUpdate();
 			
@@ -103,7 +125,7 @@ public class EmpAuthJDBCDAO implements EmpAuthDAO_interface{
 		
 	}
 	@Override
-	public void delete(String emp_id, String auth_id) {
+	public void delete(String emp_id) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		
@@ -113,7 +135,6 @@ public class EmpAuthJDBCDAO implements EmpAuthDAO_interface{
 			pstmt = con.prepareStatement(DELETE);
 			
 			pstmt.setString(1, emp_id);
-			pstmt.setString(2, auth_id);
 			pstmt.executeUpdate();
 			
 			
@@ -142,8 +163,8 @@ public class EmpAuthJDBCDAO implements EmpAuthDAO_interface{
 		
 	}
 	@Override
-	public EmpAuthVO findByPrimaryKey(String emp_id, String auth_id) {
-		EmpAuthVO empAuthVO = null;
+	public EmpVO findByPrimaryKey(String emp_id) {
+		EmpVO empVO = null;
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -152,13 +173,22 @@ public class EmpAuthJDBCDAO implements EmpAuthDAO_interface{
 			con = DriverManager.getConnection(url, userid, passwd);
 			pstmt = con.prepareStatement(GET_ONE_STMT);
 			pstmt.setString(1, emp_id);
-			pstmt.setString(2, auth_id);
+			
 			rs = pstmt.executeQuery();
 			
 			while(rs.next()) {
-				empAuthVO = new EmpAuthVO();
-				empAuthVO.setEmp_id(rs.getString("emp_id"));
-				empAuthVO.setAuth_id(rs.getString("auth_id"));			
+				empVO = new EmpVO();
+				empVO.setEmp_id(rs.getString("emp_id"));
+				empVO.setEmp_pw(rs.getString("emp_pw"));
+				empVO.setEmp_name(rs.getString("emp_name"));
+				empVO.setEmp_bday(rs.getDate("emp_bday"));
+				empVO.setEmp_email(rs.getString("emp_email"));
+				empVO.setEmp_pho(rs.getString("emp_pho"));
+				empVO.setEmp_gend(rs.getString("emp_gend"));
+				empVO.setEmp_pic(rs.getBytes("emp_pic"));
+				empVO.setEmp_state(rs.getInt("emp_state"));
+				empVO.setEmp_bday(rs.getDate("emp_bday"));
+				empVO.setEmp_address(rs.getString("emp_address"));
 			}
 						
 		} catch (ClassNotFoundException e) {
@@ -192,12 +222,12 @@ public class EmpAuthJDBCDAO implements EmpAuthDAO_interface{
 				}
 			}
 		} 
-		return empAuthVO;
+		return empVO;
 	}
 	@Override
-	public List<EmpAuthVO> getAll() {
-		List<EmpAuthVO> list = new ArrayList<>();
-		EmpAuthVO empAuthVO = null;
+	public List<EmpVO> getAll() {
+		List<EmpVO> list = new ArrayList<>();
+		EmpVO empVO = null;
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -208,11 +238,20 @@ public class EmpAuthJDBCDAO implements EmpAuthDAO_interface{
 			rs = pstmt.executeQuery();
 			
 			while(rs.next()) {
-				empAuthVO = new EmpAuthVO();
-				empAuthVO.setEmp_id(rs.getString("emp_id"));
-				empAuthVO.setAuth_id(rs.getString("auth_id"));
-				
-				list.add(empAuthVO);
+				empVO = new EmpVO();
+				empVO.setEmp_id(rs.getString("emp_id"));
+				empVO.setEmp_pw(rs.getString("emp_pw"));
+				empVO.setEmp_name(rs.getString("emp_name"));
+				empVO.setEmp_bday(rs.getDate("emp_bday"));
+				empVO.setEmp_email(rs.getString("emp_email"));
+				empVO.setEmp_pho(rs.getString("emp_pho"));
+				empVO.setEmp_gend(rs.getString("emp_gend"));
+				empVO.setEmp_pic(rs.getBytes("emp_pic"));
+				empVO.setEmp_state(rs.getInt("emp_state"));
+				empVO.setEmp_bday(rs.getDate("emp_bday"));
+				empVO.setEmp_address(rs.getString("emp_address"));
+			
+				list.add(empVO);
 			}
 			
 			
