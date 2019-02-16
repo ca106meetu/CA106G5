1ffbe2c0c217f283ff3878cec170f84a883b6db0
Administrator <Administrator@DESKTOP-88LTD4M>
2019-02-17 02:53:03 +0800

    Loren listAllProd.jsp DBText

--- WebContent/FrontEnd/prod/liistAllProd.jsp
+++ WebContent/FrontEnd/prod/liistAllProd.jsp
@@ -18,18 +18,33 @@
 
 
 <style>
-	img{
-		width:345px;
-		height:460px;
+	.pic{
+		width:172.5px;
+		height:230px;
 	}
+	table {
+	width: 800px;
+	background-color: white;
+	margin-top: 5px;
+	margin-bottom: 5px;
+  }
+  table, th, td {
+    border: 1px solid #CCCCFF;
+  }
+  th, td {
+    padding: 2px;
+    text-align: center;
+  }
 	
 </style>
 </head>
 <body bgcolor = 'gray'>
 
-<%-- <table id = 'table-1'>
+<h4>此頁練習採用 EL 的寫法取值:</h4>
+ <table id = 'table-1'>
 	<tr><td>
-	
+		<h3>所有商品資料-listAllProd.jsp</h3>
+		<h4><a href='#'><img src="images/back1.gif" width="100" height="32">回首頁</a></h4>
 	
 	
 	</td>
@@ -41,7 +56,7 @@
 
 
 </table>
---%>
+
 
 
 <table>
@@ -55,33 +70,42 @@
 		<th>促銷狀態</th>
 		<th>上架狀態</th>
 		<th>商品資訊</th>
+		<th>修改</th>
+		<th>刪除</th>		
 	</tr>
-	<c:forEach var="prodVO" items= "${list }">
+	<%@ include file="page1.file" %> 
+	<c:forEach var="prodVO" items= "${list}" begin="<%=pageIndex%>" end="<%=pageIndex+rowsPerPage-1%>">
 		<tr>
 			<td>${prodVO.prod_ID}</td>
 			<td>${prodVO.prod_name}</td>
 			<td>${prodVO.prod_price}</td>
 			<td>${prodVO.prod_type}</td>
 			<td>${prodVO.prod_stock}</td>
-			<td><img src='/CA106G5/ShowPic?PROD_ID=${prodVO.prod_ID}'></td>
+			<td><img class='pic' src='/CA106G5/ShowPic?PROD_ID=${prodVO.prod_ID}'></td>
 			<td>${prodVO.prod_promt_status}</td>
 			<td>${prodVO.prod_status}</td>
 			<td>${prodVO.prod_info}</td>
+			<td>
+				<form method='post' action='' style="margin-bottom: 0px;">
+					<input type='submit' value='修改'>
+					<input type='hidden' name='prod_ID' value='${prodVO.prod_ID }'>
+					<input type='hidden' name='action' value='getOne_For_Update'>				
+				</form></td>
+			<td>	
+				<form method='post' action='' style="margin-bottom: 0px;">
+					<input type='submit' value='刪除'>
+					<input type='hidden' name='prod_ID' value='${prodVO.prod_ID }'>
+					<input type='hidden' name='action' value='delete'>				
+				</form>
+			
+			</td>
 		
 		
 		</tr>
  	
 	</c:forEach>
 </table>
-
-
-
-
-
-
-
-
-
+<%@ include file="page2.file" %> 
 
 
 
--- src/com/meetU/product/model/ShowPic.java
+++ src/com/meetU/product/model/ShowPic.java
@@ -5,6 +5,7 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.naming.Context;
@@ -47,30 +48,63 @@ public class ShowPic extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		Integer eqPos = req.getQueryString().indexOf("=");
+		String column = req.getQueryString().substring(0, eqPos);
+		
 		res.setContentType("image/gif");
 		ServletOutputStream out = res.getOutputStream();
-		try {
-			con = ds.getConnection();
-			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery(
-				"SELECT PROD_PIC FROM PRODUCT WHERE PROD_ID = '"+ req.getParameter("PROD_ID")+"'");
-			System.out.println("---");
-
-			if (rs.next()) {
-				BufferedInputStream in = new BufferedInputStream(rs.getBinaryStream("PROD_PIC"));
-				byte[] buf = new byte[4 * 1024]; // 4K buffer
-				int len;
-				while ((len = in.read(buf)) != -1) {
-					out.write(buf, 0, len);
+		Statement stmt = null;
+		ResultSet rs = null;
+		BufferedInputStream in = null;
+		
+		if ("PROD_ID".equals(column)) {
+			try {
+				con = ds.getConnection();
+				stmt = con.createStatement();
+				rs = stmt.executeQuery(
+					"SELECT PROD_PIC FROM PRODUCT WHERE PROD_ID = '"+ req.getParameter(column)+"'");
+	
+				if (rs.next()) {
+					in = new BufferedInputStream(rs.getBinaryStream("PROD_PIC"));
+					byte[] buf = new byte[4 * 1024]; // 4K buffer
+					int len;
+					while ((len = in.read(buf)) != -1) {
+						out.write(buf, 0, len);
+					}
+				} else {
+					res.sendError(HttpServletResponse.SC_NOT_FOUND);
 				}
-				in.close();
-			} else {
-				res.sendError(HttpServletResponse.SC_NOT_FOUND);
+			} catch (Exception e) {
+				System.out.println(e);
+			} finally {
+				if(in != null) 
+					in.close();
+				if(rs != null) {
+					try {
+						rs.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				if(stmt != null) {
+					try {
+						stmt.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+				if (con != null) {
+					try {
+						con.close();
+					} catch (Exception e) {
+						e.printStackTrace(System.err);
+					}
+				}
+				
 			}
-			rs.close();
-			stmt.close();
-		} catch (Exception e) {
-			System.out.println(e);
 		}
 		
 	}
