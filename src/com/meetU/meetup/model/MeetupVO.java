13953126200d790d2763c68fcdc996e5792feda0
Java <minmushroom@gmail.com>
2019-02-16 22:50:45 +0800

    for hk

--- src/com/meetU/meetup/model/MeetupVO.java
+++ src/com/meetU/meetup/model/MeetupVO.java
@@ -1,61 +1,61 @@
 package com.meetU.meetup.model;
 
 public class MeetupVO implements java.io.Serializable{
-	private String meetupId;
-	private String meetupName;
-	private String memId;
-	private String meetupDate;
-	private String meetupLoc;
-	private Integer meetupStatus;
-	private byte[] meetupPic;
-	private String meetupInfo;
+	private String meetup_ID;
+	private String meetup_name;
+	private String mem_ID;
+	private String meetup_date;
+	private String meetup_loc;
+	private Integer meetup_status;
+	private byte[] meetup_pic;
+	private String meetup_info;
 	
-	public String getMeetupId() {
-		return meetupId;
+	public String getMeetup_ID() {
+		return meetup_ID;
 	}
-	public void setMeetupId(String meetupId) {
-		this.meetupId = meetupId;
+	public void setMeetup_ID(String meetup_ID) {
+		this.meetup_ID = meetup_ID;
 	}
-	public String getMeetupName() {
-		return meetupName;
+	public String getMeetup_name() {
+		return meetup_name;
 	}
-	public void setMeetupName(String meetupName) {
-		this.meetupName = meetupName;
+	public void setMeetup_name(String meetup_name) {
+		this.meetup_name = meetup_name;
 	}
-	public String getMemId() {
-		return memId;
+	public String getMem_ID() {
+		return mem_ID;
 	}
-	public void setMemId(String memId) {
-		this.memId = memId;
+	public void setMem_ID(String mem_ID) {
+		this.mem_ID = mem_ID;
 	}
-	public String getMeetupDate() {
-		return meetupDate;
+	public String getMeetup_date() {
+		return meetup_date;
 	}
-	public void setMeetupDate(String meetupDate) {
-		this.meetupDate = meetupDate;
+	public void setMeetup_date(String meetup_date) {
+		this.meetup_date = meetup_date;
 	}
-	public String getMeetupLoc() {
-		return meetupLoc;
+	public String getMeetup_loc() {
+		return meetup_loc;
 	}
-	public void setMeetupLoc(String meetupLoc) {
-		this.meetupLoc = meetupLoc;
+	public void setMeetup_loc(String meetup_loc) {
+		this.meetup_loc = meetup_loc;
 	}
-	public Integer getMeetupStatus() {
-		return meetupStatus;
+	public Integer getMeetup_status() {
+		return meetup_status;
 	}
-	public void setMeetupStatus(Integer meetupStatus) {
-		this.meetupStatus = meetupStatus;
+	public void setMeetup_status(Integer meetup_status) {
+		this.meetup_status = meetup_status;
 	}
-	public byte[] getMeetupPic() {
-		return meetupPic;
+	public byte[] getMeetup_pic() {
+		return meetup_pic;
 	}
-	public void setMeetupPic(byte[] meetupPic) {
-		this.meetupPic = meetupPic;
+	public void setMeetup_pic(byte[] meetup_pic) {
+		this.meetup_pic = meetup_pic;
 	}
-	public String getMeetupInfo() {
-		return meetupInfo;
+	public String getMeetup_info() {
+		return meetup_info;
 	}
-	public void setMeetupInfo(String meetupInfo) {
-		this.meetupInfo = meetupInfo;
-	}	
+	public void setMeetup_info(String meetup_info) {
+		this.meetup_info = meetup_info;
+	}
 }
