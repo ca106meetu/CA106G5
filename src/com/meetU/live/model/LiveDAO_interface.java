052d27a6f96a959544a302b17368c5a87ddb6d4f
user <user@MSI>
2019-02-18 22:21:53 +0800

    +++++

---
+++ src/com/meetU/live/model/LiveDAO_interface.java
@@ -0,0 +1,20 @@
+package com.meetU.live.model;
+
+import java.util.*;
+
+public interface LiveDAO_interface {
+
+	public void insert(LiveVO liveVO);
+
+	public void update(LiveVO liveVO);
+
+	public void delete(LiveVO liveVO);
+
+	public LiveVO findByPrimaryKey(String host_ID);
+
+	public List<LiveVO> getALL();
+
+//  萬用複合查詢(傳入參數型態Map)(回傳 List)
+//  public List<LiveVO> getAll(Map<String, String[]> map); 
+
+}
---
+++ src/com/meetU/live/model/LiveJDBCDAO.java
@@ -0,0 +1,77 @@
+package com.meetU.live.model;
+
+import java.sql.*;
+import java.util.List;
+
+public class LiveJDBCDAO implements LiveDAO_interface {
+
+	String drive = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	String userid = "CA106G5";
+	String passwd = "123456";
+
+	private static final String INSERT_STMT = "INSERT INTO LIVE (HOST_ID, LIVE_NAME, LIVE_ACC, LIVE_PIC, LIVE_DATE, LIVE_STATUS) VALUES (?,?,?,?,?,?)";
+	private static final String UPDATE = "UPDATE LIVE set LIVE_NAME = ?, LIVE_ACC = ?, LIVE_PIC = ?, LIVE_STATUS = ?  where HOST_ID = ?";
+	private static final String GET_ALL_STMT = "SELECT * FROM LIVE";
+	private static final String GET_ONE_STMT = "SELECT * FROM LIVE where HOST_ID = ?";
+	private static final String DELETE = "DELETE FROM LIVE where HOST_ID = ?";
+
+	@Override
+	public void insert(LiveVO liveVO) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			Class.forName(drive);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(INSERT_STMT);
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void update(LiveVO liveVO) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(LiveVO liveVO) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public LiveVO findByPrimaryKey(String host_ID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<LiveVO> getALL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
