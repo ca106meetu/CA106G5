5f1406a382500443dd04e8b291952b66d45e0393
jack <gogo0931@gmail.com>
2019-03-24 23:46:08 +0800

    jj

--- src/com/meetU/toolClass/InitializeStatus.java
+++ src/com/meetU/toolClass/InitializeStatus.java
@@ -14,6 +14,8 @@ public class InitializeStatus implements ServletContextListener{
 	private List<String> ps;
 	private List<String> outs;
 	private List<String> ords;
+	private List<String> mS;
+	private List<String> eS;
 	private String[] arrayCity = {"台北市", "基隆市", "新北市", "連江縣", "宜蘭縣", "釣魚台", "新竹市", "新竹縣", "桃園市", "苗栗縣", "台中市", 
 	           	"彰化縣", "南投縣", "嘉義市", "嘉義縣", "雲林縣", "台南市", "高雄市", "南海島", "澎湖縣", "金門縣", "屏東縣", "台東縣", "花蓮縣"};
 	private List<String> listCity = Arrays.asList(arrayCity);
@@ -50,12 +52,24 @@ public class InitializeStatus implements ServletContextListener{
 		ords.add(0,"待出貨");
 		ords.add(1,"出貨中");
 		ords.add(2,"已到貨");
+
+		mS =  new ArrayList<>();
+		mS.add(0, "尚未驗證");
+		mS.add(1, "普通會員");
 		
+		eS = new ArrayList<>();
+		eS.add(0, "尚未開通");
+		eS.add(1, "已開通");
+		eS.add(2, "離職");
+				
 		context.setAttribute("pt", pt);
 		context.setAttribute("ps", ps);
 		context.setAttribute("pps", pps);
 		context.setAttribute("outs", outs);
 		context.setAttribute("ords", ords);
+		
+		context.setAttribute("mS", mS);
+		context.setAttribute("eS", eS);
 		
 		context.setAttribute("listCity", listCity);
 		System.out.println("阿彌陀佛阿門阿拉~BUG不要來");
