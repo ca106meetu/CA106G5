e88643373890387febd755fa5a8051f68f156ac9
jack <gogo0931@gmail.com>
2019-03-26 01:09:00 +0800

    jackhsu190326V0112

--- src/com/meetU/frontLogin/FrontLoginHandler.java
+++ src/com/meetU/frontLogin/FrontLoginHandler.java
@@ -53,7 +53,7 @@ public class FrontLoginHandler extends HttpServlet {
 				
 				MemHobbyService memHobbySvc = new MemHobbyService();
 				List<String> listHobby_ID = memHobbySvc.getPartOfOneMemHobby2(mem_ID);
-				session.setAttribute("listHobby_ID", listHobby_ID);
+				//session.setAttribute("listHobby_ID", listHobby_ID);
 				return true;
 			}
 		} else {
@@ -220,33 +220,11 @@ public class FrontLoginHandler extends HttpServlet {
 			regMemVO = mRegAccSvc.regInsert(register_mem_pw, register_mem_acc, register_mem_email, register_mem_addr);
 			session.setAttribute("regMemVO", regMemVO);
 			// String url = "/FrontEnd/mem/reg_mem_input.jsp";
-			String url = "/FrontEnd/mem/Email.jsp";
+			String url = "/EmailValidator";
 			RequestDispatcher successView = req.getRequestDispatcher(url);// 成功轉交 update_mem_input.jsp
 			successView.forward(req, res);
 			return;
 		}
-//		if ("login".equals(action)) {
-//			// 【檢查該帳號 , 密碼是否有效】
-//			if (!allowUser(mem_acc, mem_pw, session, req, res)) { // 【帳號 , 密碼無效時】
-//				out.print("<!-- -->");
-//				out.close();
-//
-//			} else { // 【帳號 , 密碼有效時, 才做以下工作】
-//				session.setAttribute("mem_acc", mem_acc); // *工作1: 才在session內做已經登入過的標識
-//
-//				try {
-//					JSONObject obj0 = new JSONObject();
-//					obj0.accumulate("access", "true");
-//					out.print(obj0.toString());
-//					out.flush();
-//					out.close();
-//				} catch (Exception ignored) {
-//
-//				}
-//
-//			}
-//			return;
-//		}
 
 		// 【檢查該帳號 , 密碼是否有效】
 		if (!allowUser(mem_acc, mem_pw, session, req, res)) { // 【帳號 , 密碼無效時】
@@ -275,25 +253,7 @@ public class FrontLoginHandler extends HttpServlet {
 		}
 	}
 
-	private static String returnAuthCode() {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i <= 8; i++) {
-			int condition = (int) (Math.random() * 3) + 1;
-			switch (condition) {
-			case 1:
-				char c1 = (char) ((int) (Math.random() * 26) + 65);
-				sb.append(c1);
-				break;
-			case 2:
-				char c2 = (char) ((int) (Math.random() * 26) + 97);
-				sb.append(c2);
-				break;
-			case 3:
-				sb.append((int) (Math.random() * 10));
-			}
-		}
-		return sb.toString();
-	}
+	
 
 	public String getFileNameFromPart(Part part) {
 		String header = part.getHeader("content-disposition");
@@ -306,48 +266,5 @@ public class FrontLoginHandler extends HttpServlet {
 		return filename;
 	}
 
-	public class MailService {
-
-		// 設定傳送郵件:至收信人的Email信箱,Email主旨,Email內容
-		public void sendMail(String to, String subject, String messageText) {
-
-			try {
-				// 設定使用SSL連線至 Gmail smtp Server
-				Properties props = new Properties();
-				props.put("mail.smtp.host", "smtp.gmail.com");
-				props.put("mail.smtp.socketFactory.port", "465");
-				props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-				props.put("mail.smtp.auth", "true");
-				props.put("mail.smtp.port", "465");
-
-				// ●設定 gmail 的帳號 & 密碼 (將藉由你的Gmail來傳送Email)
-				// ●須將myGmail的【安全性較低的應用程式存取權】打開
-//			     final String myGmail = "ixlogic.wu@gmail.com";
-//			     final String myGmail_password = "BBB45678";
-				final String myGmail = "jack1915tw@gmail.com";
-				final String myGmail_password = "8812qqqq";
-				Session session = Session.getInstance(props, new Authenticator() {
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(myGmail, myGmail_password);
-					}
-				});
-
-				Message message = new MimeMessage(session);
-				message.setFrom(new InternetAddress(myGmail));
-				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-
-				// 設定信中的主旨
-				message.setSubject(subject);
-				// 設定信中的內容
-				message.setText(messageText);
-
-				Transport.send(message);
-				//System.out.println("傳送成功!");
-			} catch (MessagingException e) {
-				//System.out.println("傳送失敗!");
-				e.printStackTrace();
-			}
-		}
-	}
-
+	
 }
