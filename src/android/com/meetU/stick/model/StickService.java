0a2def9548914c46be34ca0314f206b7ee229f0e
user <user@LAPTOP-3GOE5JFV>
2019-03-27 14:30:15 +0800

    android package Stick

---
+++ src/android/com/meetU/stick/model/StickService.java
@@ -0,0 +1,32 @@
+package android.com.meetU.stick.model;
+
+import java.util.List;
+
+import android.com.meetU.stick.model.StickVO;
+
+public class StickService {
+
+	private StickDAO_interface dao;
+
+	public StickService() {
+		dao = new StickDAO();
+	}
+
+//	條件查詢
+	public StickVO getOneStick(String stick_ID) {
+		StickVO stickVO = dao.findByPrimaryKey(stick_ID);
+		return stickVO;
+	}
+
+//	查詢全部
+	public List<StickVO> getAll() {
+		List<StickVO> list = dao.getALL();
+		return list;
+
+	}
+	
+	public byte[] getImage(String stick_ID) {
+		return dao.getImage(stick_ID);
+	}
+
+}
