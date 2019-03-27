e1ded69b1cad46a8c1227dd5d9f041c8ea05c669
user <user@MSI>
2019-03-28 03:31:09 +0800

    改GOOGLE帳號

--- src/tools/MailService.java
+++ src/tools/MailService.java
@@ -26,10 +26,10 @@ public class MailService {
 
        // ●設定 gmail 的帳號 & 密碼 (將藉由你的Gmail來傳送Email)
        // ●須將myGmail的【安全性較低的應用程式存取權】打開
-	     final String myGmail = "ixlogic.wu@gmail.com"; 
-	     final String myGmail_password = "BBB45678";
-//	     final String myGmail = "jack1915tw@gmail.com";
-//	     final String myGmail_password = "8812qqqq";
+//	     final String myGmail = "ixlogic.wu@gmail.com"; 
+//	     final String myGmail_password = "BBB45678";
+	     final String myGmail = "jack1915tw@gmail.com";
+	     final String myGmail_password = "8812qqqq";
 	     Session session = Session.getInstance(props, new Authenticator() {
 			   protected PasswordAuthentication getPasswordAuthentication() {
 				   return new PasswordAuthentication(myGmail, myGmail_password);
